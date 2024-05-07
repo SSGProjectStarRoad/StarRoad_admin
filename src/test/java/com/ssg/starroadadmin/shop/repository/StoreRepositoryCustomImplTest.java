a5de7044c4b32d6807788ac9a3f64e7f31a25894
jjuuuunn <didtdjwns111@gmail.com>
2024-05-07 23:22:25 +0900

    FEAT : Write SearchStoreList Test Code

--- src/test/java/com/ssg/starroadadmin/shop/repository/StoreRepositoryCustomImplTest.java
+++ src/test/java/com/ssg/starroadadmin/shop/repository/StoreRepositoryCustomImplTest.java
@@ -26,43 +26,30 @@ class StoreRepositoryCustomImplTest {
 
     @Autowired
     private StoreRepository storeRepository;
-    @Autowired ComplexShoppingmallRepository complexShoppingmallRepository;
+    @Autowired
+    ComplexShoppingmallRepository complexShoppingmallRepository;
 
     static ComplexShoppingmall shoppingmall;
 
     @BeforeEach
     void init() {
         shoppingmall = complexShoppingmallRepository.save(ComplexShoppingmall.builder().build());
-        Store store1_1 = Store.builder()
-                .complexShoppingmall(shoppingmall)
-                .name("테스트 상점1_1")
-                .storeType("테스트 타입1")
-                .floor(1)
-                .build();
-        Store store1_2 = Store.builder()
-                .complexShoppingmall(shoppingmall)
-                .name("테스트 상점1_2")
-                .storeType("테스트 타입2")
-                .floor(1)
-                .build();
-        Store store2_1 = Store.builder()
-                .complexShoppingmall(shoppingmall)
-                .name("테스트 상점2_1")
-                .storeType("테스트 타입1")
-                .floor(2)
-                .build();
-        Store store2_2 = Store.builder()
-                .complexShoppingmall(shoppingmall)
-                .name("테스트 상점2_2")
-                .storeType("테스트 타입2")
-                .floor(2)
-                .build();
 
-        // 상점 데이터를 저장
-        storeRepository.save(store1_1);
-        storeRepository.save(store1_2);
-        storeRepository.save(store2_1);
-        storeRepository.save(store2_2);
+        for (int i = 0; i < 20; i++) {
+            storeRepository.save(Store.builder()
+                    .name(String.format("테스트 상점1_%02d", i))
+                    .storeType("테스트 타입" + (i % 2))
+                    .floor(1)
+                    .complexShoppingmall(shoppingmall)
+                    .build());
+
+            storeRepository.save(Store.builder()
+                    .name(String.format("테스트 상점2_%02d", i))
+                    .storeType("테스트 타입" + (i % 2))
+                    .floor(2)
+                    .complexShoppingmall(shoppingmall)
+                    .build());
+        }
     }
 
     @AfterEach
@@ -81,28 +68,28 @@ class StoreRepositoryCustomImplTest {
         // when
         // 1. 1층 상점 목록 조회
         Page<StoreListResponse> storeList1 = storeRepositoryCustom.findByComplexShoppingmallIdAndNameContainingAndFloorAndStoreType(
-                shoppingmall.getId(),null, 1, "테스트 타입1", sortType, pageable);
+                shoppingmall.getId(), null, 1, "테스트 타입1", sortType, pageable);
 
         // 2. 2층 상점 목록 조회
         Page<StoreListResponse> storeList2 = storeRepositoryCustom.findByComplexShoppingmallIdAndNameContainingAndFloorAndStoreType(
-                shoppingmall.getId(),null, 2, "테스트 타입2", sortType, pageable);
+                shoppingmall.getId(), null, 2, "테스트 타입0", sortType, pageable);
 
         // then
         // when 1 검증
         assertThat(storeList1).isNotNull();
-        assertThat(storeList1.getTotalElements()).isEqualTo(1);
+        assertThat(storeList1.getTotalElements()).isEqualTo(10);
         List<StoreListResponse> content1 = storeList1.getContent();
-        assertThat(content1).extracting("name").containsExactly("테스트 상점1_1");
-        assertThat(content1).extracting("storeType").containsExactly("테스트 타입1");
-        assertThat(content1).extracting("floor").containsExactly( 1);
+        assertThat(content1).extracting("name").containsExactly("테스트 상점1_01", "테스트 상점1_03", "테스트 상점1_05", "테스트 상점1_07", "테스트 상점1_09", "테스트 상점1_11", "테스트 상점1_13", "테스트 상점1_15", "테스트 상점1_17", "테스트 상점1_19");
+        assertThat(content1).extracting("storeType").containsExactly("테스트 타입1", "테스트 타입1", "테스트 타입1", "테스트 타입1", "테스트 타입1", "테스트 타입1", "테스트 타입1", "테스트 타입1", "테스트 타입1", "테스트 타입1");
+        assertThat(content1).extracting("floor").containsExactly(1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
 
         // when 2 검증
         assertThat(storeList2).isNotNull();
-        assertThat(storeList2.getTotalElements()).isEqualTo(1);
+        assertThat(storeList2.getTotalElements()).isEqualTo(10);
         List<StoreListResponse> content2 = storeList2.getContent();
-        assertThat(content2).extracting("name").containsExactly("테스트 상점2_2");
-        assertThat(content2).extracting("storeType").containsExactly("테스트 타입2");
-        assertThat(content2).extracting("floor").containsExactly(2);
+        assertThat(content2).extracting("name").containsExactly("테스트 상점2_00", "테스트 상점2_02", "테스트 상점2_04", "테스트 상점2_06", "테스트 상점2_08", "테스트 상점2_10", "테스트 상점2_12", "테스트 상점2_14", "테스트 상점2_16", "테스트 상점2_18");
+        assertThat(content2).extracting("storeType").containsExactly("테스트 타입0", "테스트 타입0", "테스트 타입0", "테스트 타입0", "테스트 타입0", "테스트 타입0", "테스트 타입0", "테스트 타입0", "테스트 타입0", "테스트 타입0");
+        assertThat(content2).extracting("floor").containsExactly(2, 2, 2, 2, 2, 2, 2, 2, 2, 2);
     }
 
     @Test
@@ -114,25 +101,72 @@ class StoreRepositoryCustomImplTest {
         Pageable pageable = PageRequest.of(0, 10);
 
         // when
-        // 1. name이 "1_1"이고 1층에 있는 상점 목록 조회
+        // 1. name이 "테스트 상점"이고 1층에 있는 상점 목록 조회
         Page<StoreListResponse> storeList1 = storeRepositoryCustom.findByComplexShoppingmallIdAndNameContainingAndFloorAndStoreType(
-                shoppingmall.getId(),"1_1", 1, "테스트 타입1", sortType, pageable);
+                shoppingmall.getId(), "테스트 상점", 1, "테스트 타입1", sortType, pageable);
 
-        // 2. name이 "1_2"이고 2층에 있는 상점 목록 조회(검색 결과 없음)
+        // 2. name이 "상점2"이고 2층에 있는 상점 목록 조회(검색 결과 없음)
         Page<StoreListResponse> storeList2 = storeRepositoryCustom.findByComplexShoppingmallIdAndNameContainingAndFloorAndStoreType(
-                shoppingmall.getId(),"1_2", 2, "테스트 타입2", sortType, pageable);
+                shoppingmall.getId(), "상점2", 2, "테스트 타입0", sortType, pageable);
 
         // then
         // when 1 검증
         assertThat(storeList1).isNotNull();
-        assertThat(storeList1.getTotalElements()).isEqualTo(1);
+        assertThat(storeList1.getTotalElements()).isEqualTo(10);
         List<StoreListResponse> content1 = storeList1.getContent();
-        assertThat(content1).extracting("name").containsExactly("테스트 상점1_1");
-        assertThat(content1).extracting("storeType").containsExactly("테스트 타입1");
-        assertThat(content1).extracting("floor").containsExactly(1);
+        assertThat(content1).extracting("name").containsExactly("테스트 상점1_01", "테스트 상점1_03", "테스트 상점1_05", "테스트 상점1_07", "테스트 상점1_09", "테스트 상점1_11", "테스트 상점1_13", "테스트 상점1_15", "테스트 상점1_17", "테스트 상점1_19");
+        assertThat(content1).extracting("storeType").containsExactly("테스트 타입1", "테스트 타입1", "테스트 타입1", "테스트 타입1", "테스트 타입1", "테스트 타입1", "테스트 타입1", "테스트 타입1", "테스트 타입1", "테스트 타입1");
+        assertThat(content1).extracting("floor").containsExactly(1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
 
         // when 2 검증
         assertThat(storeList2).isNotNull();
-        assertThat(storeList2.getTotalElements()).isEqualTo(0);
+        assertThat(storeList2.getTotalElements()).isEqualTo(10);
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("상점 목록 조회 성공 테스트 - (name : not null, floor : 0)")
+    public void givenNameIsNotNullAndFloorIsZeroAndStoreTypeAndPageable_whenFindByNameContainingOrFloorOrStoreType_thenStoreListIsFound() {
+        // given
+        StoreSortType sortType = NAME_ASC;
+        Pageable pageable = PageRequest.of(0, 10);
+
+        // when
+        // 1. name이 "테스트 상점"이고 1층에 있는 상점 목록 조회
+        Page<StoreListResponse> storeList1 = storeRepositoryCustom.findByComplexShoppingmallIdAndNameContainingAndFloorAndStoreType(
+                shoppingmall.getId(), "테스트 상점", 0, "테스트 타입1", sortType, pageable);
+
+        // 2. name이 "상점2"이고 2층에 있는 상점 목록 조회(검색 결과 없음)
+        Page<StoreListResponse> storeList2 = storeRepositoryCustom.findByComplexShoppingmallIdAndNameContainingAndFloorAndStoreType(
+                shoppingmall.getId(), "상점2", 0, "테스트 타입0", sortType, pageable);
+
+        // then
+        // when 1 검증
+        assertThat(storeList1).isNotNull();
+        assertThat(storeList1.getTotalElements()).isEqualTo(20);
+        List<StoreListResponse> content1 = storeList1.getContent();
+        assertThat(content1).extracting("name").containsExactly("테스트 상점1_01", "테스트 상점1_03", "테스트 상점1_05", "테스트 상점1_07", "테스트 상점1_09", "테스트 상점1_11", "테스트 상점1_13", "테스트 상점1_15", "테스트 상점1_17", "테스트 상점1_19");
+        assertThat(content1).extracting("storeType").containsExactly("테스트 타입1", "테스트 타입1", "테스트 타입1", "테스트 타입1", "테스트 타입1", "테스트 타입1", "테스트 타입1", "테스트 타입1", "테스트 타입1", "테스트 타입1");
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("상점 목록 조회 성공 테스트 - (name : null, floor : 0, storeType : null)")
+    public void givenNameIsNullAndFloorIsZeroAndStoreTypeIsNullAndPageable_whenFindByNameContainingOrFloorOrStoreType_thenStoreListIsFound() {
+        // given
+        StoreSortType sortType = NAME_ASC;
+        Pageable pageable = PageRequest.of(0, 10);
+
+        // when
+        // 1. name이 "테스트 상점"이고 1층에 있는 상점 목록 조회
+        Page<StoreListResponse> storeList = storeRepositoryCustom.findByComplexShoppingmallIdAndNameContainingAndFloorAndStoreType(
+                shoppingmall.getId(), null, 0, null, sortType, pageable);
+
+        // then
+        // when 1 검증
+        assertThat(storeList).isNotNull();
+        assertThat(storeList.getTotalElements()).isEqualTo(40);
+        List<StoreListResponse> content = storeList.getContent();
+        assertThat(content).extracting("name").containsExactly("테스트 상점1_00", "테스트 상점1_01", "테스트 상점1_02", "테스트 상점1_03", "테스트 상점1_04", "테스트 상점1_05", "테스트 상점1_06", "테스트 상점1_07", "테스트 상점1_08", "테스트 상점1_09");
     }
 }
