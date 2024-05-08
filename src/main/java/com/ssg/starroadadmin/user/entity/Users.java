80276dce9671e7e2ad8767f6b578198ae69a52e6
jjuuuunn <didtdjwns111@gmail.com>
2024-05-08 19:36:33 +0900

    HOTFIX : Update User Table Fields
    username 필드를 제거하고 provider 를 조금 더 세분화 하여
    type 과 id 로 분리하여 저장하도록 변경하고
    type은 Google, Naver, Kakao OAuth만
    사용할 예정임으로 열거형으로 설정

--- src/main/java/com/ssg/starroadadmin/user/entity/Users.java
+++ src/main/java/com/ssg/starroadadmin/user/entity/Users.java
@@ -3,6 +3,7 @@ package com.ssg.starroadadmin.user.entity;
 import com.ssg.starroadadmin.global.entity.BaseTimeEntity;
 import com.ssg.starroadadmin.user.enums.ActiveStatus;
 import com.ssg.starroadadmin.user.enums.Gender;
+import com.ssg.starroadadmin.user.enums.ProviderType;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,26 +22,25 @@ public class Users extends BaseTimeEntity {
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
-    @Column(unique = true)
-    private String username;
-    private String password; // bcrypt
+    private String password;
     private String name;
     @Column(unique = true)
     private String nickname;
-    private String imagePath; // profile image
     @Enumerated(EnumType.STRING)
     private Gender gender;
     @Column
     private LocalDate birth;
-    private String phone; // xxx-xxxx-xxxx
+    private String phone;
     @Column(unique = true)
     private String email;
-    @Column(unique = true)
-    private String provider; // provider + providerId => unique
+    private String imagePath;
 
     private int reviewExp;
     private int point;
 
     @Enumerated(EnumType.STRING)
     private ActiveStatus activeStatus;
+
+    private ProviderType providerType; // 네이버, 카카오, 구글
+    private String providerId; // (네이버, 카카오, 구글)에서 받아온 아이디
 }
