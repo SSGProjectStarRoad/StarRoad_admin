db9a2bb88f8f5e8ac83ddf0d5fea20d9df299f0b
jjuuuunn <didtdjwns111@gmail.com>
2024-05-09 10:05:31 +0900

    REFACT : Change User Table Name
    User 테이블 이름 변경 users -> user

--- src/main/java/com/ssg/starroadadmin/user/repository/UserRepository.java
+++ src/main/java/com/ssg/starroadadmin/user/repository/UserRepository.java
@@ -1,8 +1,8 @@
 package com.ssg.starroadadmin.user.repository;
 
-import com.ssg.starroadadmin.user.entity.Users;
+import com.ssg.starroadadmin.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Users, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
