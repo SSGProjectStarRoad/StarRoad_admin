722e12c5a9e850c8a732dce62fac04842143306a
jjuuuunn <didtdjwns111@gmail.com>
2024-05-10 14:26:24 +0900

    FEAT : Implement S3 dependency injection and file upload component
     - S3 의존성 주입
     - S3 컴포넌트 생성(업로드, 삭제)
     - S3 테스트 코드 작성

---
+++ src/main/java/com/ssg/starroadadmin/global/S3Controller.java
@@ -0,0 +1,31 @@
+package com.ssg.starroadadmin.global;
+
+import com.ssg.starroadadmin.global.service.S3Uploader;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+public class S3Controller {
+
+    private final S3Uploader s3Uploader;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile[] file,
+                                             @RequestParam("dirName") String dirName) {
+        return ResponseEntity.ok(s3Uploader.upload(file, dirName));
+    }
+
+    @PostMapping("/delete")
+    public ResponseEntity<String> deleteFile(@RequestParam("fileName") String fileName) {
+        s3Uploader.delete(fileName);
+        return ResponseEntity.ok("File deleted successfully!");
+    }
+}
---
+++ src/main/java/com/ssg/starroadadmin/global/config/S3Config.java
@@ -0,0 +1,35 @@
+package com.ssg.starroadadmin.global.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String regionName;
+
+    @Value("${cloud.aws.s3.endpoint}")
+    private String endPoint;
+
+    @Bean
+    public AmazonS3 amazonS3() {
+        return AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endPoint, regionName))
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+                .build();
+    }
+}
